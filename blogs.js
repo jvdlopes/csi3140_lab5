30eb798468e97f6515cfbf50632985e25c32138c
jvdlopes <jvdlopes04@gmail.com>
2025-06-24 12:43:14 -0400

    figured out how to make new elements

--- blogs.js
+++ blogs.js
@@ -1,14 +1,18 @@
-function test(){
-    
-}
-//var blogList = [];
-
-
-document.writeln("<p>this is attached</p>");
+var blogList = ["<p>this</p>","<p>is</p>","<p>a</p>","<p>test</p>"];
+var printer = document.getElementById("postsContainer");
 
 const postBtn = document.getElementById("createPost");
 postBtn.addEventListener("click",addBlog);
+
+
 function addBlog(){
-    var printer = document.getElementById("postsContainer");
-    printer.innerHTML = '<h1>this is a test</h1>';
+    
+    blogList.forEach(print);
+    
+}
+
+function print(value){
+    var post = "";
+    post += '<h1>this is a test</h1>';
+    printer.innerHTML ="" + post;
 }
