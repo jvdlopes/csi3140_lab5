05be661530f3881f3143685331550320060f9178
jvdlopes <jvdlopes04@gmail.com>
2025-06-28 00:04:56 -0400

    finished

--- blogs.js
+++ blogs.js
@@ -3,8 +3,9 @@ var printer = document.getElementById("postsContainer");
 
 const postBtn = document.getElementById("createPost");
 postBtn.addEventListener("click",handleAddPost);
-//var temp = new blog("test","me","this is a test")
-blogList.push(new blog("test","me","this is a test"));
+blogList.push(new blog("The making of this blog","John","This blog site was not fun to make."));
+
+displayPosts();
 
 function handleAddPost(event){
     var title = document.getElementById("titleForm").value;
@@ -18,11 +19,9 @@ function displayPosts(){
     printer.innerHTML = "";
     var post = "";
     for (let x = 0; x < blogList.length; x++) {
-        //post += "<p>" + blogList[x].author + "</p>";
         post += "<div id = 'blogPost'> <h1 onclick=\"alert('" + blogList[x].text + "')\">" + blogList[x].title+ "</h1> <h3>by " + blogList[x].author + " on " + blogList[x].date + "</h3> <p>" + blogList[x].text + "</p></div>";
     }
     printer.innerHTML ="" + post;
-    //printer.innerHTML = "<p>" + test.author + "</p>";
 }
 
 function testPrint(){
